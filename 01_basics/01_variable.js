e80d55fdabeacbf658f1af1cef1709c0ad4531ac
RaviRanjan563 <161277558+RaviRanjan563@users.noreply.github.com>
2025-09-10 08:44:28 +0000

    variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,20 @@
+let accountId= 14453;
+let accountEmail="ravi@gmail.com";
+var accountPassword="2344";
 
+accountCity="Jaipur";
+
+accountEmail="text@gmail.com";
+accountPassword="8686";
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in the block scope and functional scope
+
+*/
+
+let accountState; // output is Undefined
+
+console.table([accountEmail, accountId,accountPassword,accountCity,accountState]);
