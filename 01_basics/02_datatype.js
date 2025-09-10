d031606cb22695eabe57cdde73c8b8299277d022
RaviRanjan563 <161277558+RaviRanjan563@users.noreply.github.com>
2025-09-10 08:43:52 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+
+"use strict"; // treat all JS code as newer version
+
+ // alert(3+3)  it will give error because it is in node.js
+
+ // use a readable format in coding
+
+ // Datatypes
+
+ let name =" Ravi";
+ let age=19;
+ let isLoggedIn = false;
+
+ // 1. number => 2 to power 53
+ // 2. bigint
+ // 3. string =>""
+ // 4. boolean => true/false
+ // 5. null => standalone value
+ // 6. undefined =>
+ // 7.symbol => to check uniqueness
+
+ // object
+
+ console.log(typeof age);
+
+ console.log(typeof null);              // output = OBJECT
+  
+ console.log(typeof undefined);          // output = UNDEFINED
